5ba92699153f45a57315e1dec4718aba0a990e7b
Ayobami <ayobami.t.owolabi@gmail.com>
2023-02-27 00:38:02 +0000

    Initial Commit

---
+++ udder/udderApp.swift
@@ -0,0 +1,17 @@
+//
+//  udderApp.swift
+//  udder
+//
+//  Created by Ayobami Owolabi on 27/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct udderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
